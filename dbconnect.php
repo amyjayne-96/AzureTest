ba2c2b26cecca811814a12c5ed8c8149b05de0bf
Amy Wilson <amyjayne-96@hotmail.co.uk>
2016-10-24 17:08:22 +0100

    Database

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_SERVER', 'servername');
-define('DB_USERNAME', 'username');
-define('DB_PASSWORD', 'password');
-define('DB_DATABASE','database');
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'b6eddf165f7163');
+define('DB_PASSWORD', 'b43888f8');
+define('DB_DATABASE','ajw1405437');
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
